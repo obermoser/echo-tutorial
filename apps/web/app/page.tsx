2dd4ef18bc234890f1670c97c6afdc54def1ff79
obermoser <bernhard.obermoser@icloud.com>
2025-08-26 19:41:45 +0200

    03: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,33 @@
 "use client";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@workspace/ui/components/button";
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
     <>
       <Authenticated>
-        <div className="flex items-center justify-center min-h-svh" >
+        <div className="flex items-center justify-center min-h-svh">
           <div className="flex flex-col items-center justify-center gap-4">
             <h1 className="text-2xl font-bold">Hello apps/web</h1>
             <UserButton />
-            <pre>
-              {users ? JSON.stringify(users, null, 2) : "Loading..."}
-            </pre>
+            <Button
+            className="cursor-pointer"
+            variant={"secondary"}
+              onClick={() => {
+                addUser();
+              }}
+            >
+              Add Test User
+            </Button>
+            <pre>{users ? JSON.stringify(users, null, 2) : "Loading..."}</pre>
           </div>
         </div>
       </Authenticated>
@@ -22,5 +36,5 @@ export default function Page() {
         <SignInButton />
       </Unauthenticated>
     </>
-  )
+  );
 }
