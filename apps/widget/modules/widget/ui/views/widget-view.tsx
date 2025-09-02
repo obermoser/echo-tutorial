e1a9770d90e90381fb502582f5cdafcd5d9b5f70
obermoser <bernhard.obermoser@icloud.com>
2025-09-02 21:33:53 +0200

    09:widget-layout

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -11,7 +11,12 @@ export const WidgetView = ({ organizationId }: Props) => {
     return (
         // TODO: Confirm wether min-h-screen/min-w-screen is needed
         <main className="flex min-h-screen min-w-screen h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetHeader>Header</WidgetHeader>
+            <WidgetHeader>
+                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
+                    <p className="font-semibold text-3xl">Hi there! 👋</p>
+                    <p className="font-semibold text-xl">How can we help you today?</p>
+                </div>
+            </WidgetHeader>
             <div className="flex flex-1">
                 Widget View: {organizationId}
             </div>
