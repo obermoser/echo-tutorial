9ef3d8a14d5533df9d8784985741640d5303229b
Bernhard <bernhard.obermoser@icloud.com>
2025-09-03 14:14:47 +0200

    Add error screen component and integrate with widget view

--- apps/widget/modules/widget/atoms/widget-atoms.ts
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -3,4 +3,6 @@ import { atom } from "jotai";
 
 //Basic widget state atoms
 
-export const screenAtom = atom<WidgetScreen>("auth");
+export const screenAtom = atom<WidgetScreen>("error");
+
+export const errorMessageAtom = atom<string | null>(null);
---
+++ apps/widget/modules/widget/ui/screens/widget-error-screen.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useAtomValue } from 'jotai';
+import React from 'react'
+import { errorMessageAtom } from '../../atoms/widget-atoms';
+import WidgetHeader from '../components/widget-header';
+import { AlertTriangleIcon } from 'lucide-react';
+
+export const WidgetErrorScreen = () => {
+    const errorMessage = useAtomValue(errorMessageAtom);
+
+    return (
+        <>
+            <WidgetHeader>
+                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
+                    <p className="font-semibold text-3xl">Hi there! 👋</p>
+                    <p className="font-semibold text-xl">Let's get you started</p>
+                </div>
+            </WidgetHeader>
+            <div className='flex flex-1 flex-col items-center justify-center gap-y-4 p-4 text-muted-foreground'>
+                <AlertTriangleIcon />
+                <p className='text-sm'>{errorMessage || "Invalid configuration"}</p>
+            </div>
+        </>
+    )
+}
